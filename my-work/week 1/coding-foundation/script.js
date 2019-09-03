ab072927f5791fd07d086b014dba447b9d98d07e
Aleksandra Medina <am7625@nyu.edu>
2019-09-03 13:19:38 +0800

    js creates a new box! 🎖

---
+++ my-work/week 1/coding-foundation/script.js
@@ -0,0 +1,3 @@
+var box = document.createElement("div");   // Create a <button> element
+box.className = "box1";                 // Insert text
+document.body.appendChild(box);               // Append <button> to <body>
