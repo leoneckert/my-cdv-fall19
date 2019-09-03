e34dff4f06af14548a78cc5b8c1c52b6df4cd3a7
Aleksandra Medina <am7625@nyu.edu>
2019-09-03 13:55:16 +0800

    creates multiple boxes🤪

--- my-work/week 1/coding-foundation/script.js
+++ my-work/week 1/coding-foundation/script.js
@@ -1,3 +1,22 @@
-var box = document.createElement("div");   // Create a <button> element
-box.className = "box1";                 // Insert text
-document.body.appendChild(box);               // Append <button> to <body>
+
+
+
+// create button on page (html)
+//✅
+
+//select the button
+var button = document.getElementById('button');
+console.log (button);
+//✅
+
+
+//detect the click
+button.addEventListener('click', createBox)
+
+
+function createBox (){
+  var box = document.createElement("div");   // Create a <button> element
+  box.className = "box1";                 // Insert text
+  document.body.appendChild(box);               // Append <button> to <body>
+
+}
